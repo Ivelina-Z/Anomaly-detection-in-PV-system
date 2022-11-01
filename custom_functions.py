437e29a872915ddc41d34dccd1c6e61f50b3cd20
Ivelina-Zhekova <iveta.zhekova@gmail.com>
2022-11-01 10:23:42 +0200

    adding custom functios
    New custom functions for plotting and exploration of the upper and lower extreme values.

--- custom_functions.py
+++ custom_functions.py
@@ -1,6 +1,10 @@
-import datetime 
+import pandas as pd
 import matplotlib.pyplot as plt
+import datetime 
+from math import ceil
 
+
+# DATA CLEANING
 def delete_additional_header(dataframe):
     ''' 
     If additional headers are present in the dataframe those will be dropped.
@@ -32,6 +36,8 @@ def drop_years(dataframe, years: list):
     return dataframe
     
 
+# PLOT 
+
 def plot_time_series(dataframe, time_range: tuple, y: list, step):
     if step == 'hour':
         time = dataframe['timestamp'][dataframe['timestamp'].dt.date == datetime.date(*time_range)]
@@ -44,7 +50,7 @@ def plot_time_series(dataframe, time_range: tuple, y: list, step):
         time = dataframe['timestamp'][dataframe['timestamp'].dt.year == time_range]
         y_feature = [dataframe[feature][dataframe['timestamp'].dt.year == time_range] for feature in y]
 
-    [plt.plot(time, feature) for feature in y_feature]
+    [plt.plot(time, feature, alpha = 0.8) for feature in y_feature]
     plt.title(f'Time series of {", ".join(y)} by {step}')
     plt.xlabel(step)
     plt.xticks(rotation=90)
@@ -56,3 +62,56 @@ def plot_time_series(dataframe, time_range: tuple, y: list, step):
         plt.ylabel(", ".join(y))
 
     plt.show()
+    
+    
+def subplot_boxplots(dataframe, columns):
+    rows = ceil(len(columns) / 3)
+    fig, ax = plt.subplots(rows, 3)
+    fig.set_figheight(30)
+    fig.set_figwidth(30)
+    for row in range(rows):
+        for col in range(3):
+            try:
+                ax[row, col].boxplot(dataframe[columns[col + row * 3]], showmeans = True, meanline=True)
+                ax[row, col].set_title(f'Boxplot for {columns[col + row * 3]}')
+            except IndexError:
+                break
+    plt.show()
+
+
+
+# DATA EXAMINATION
+
+def get_extreme_values(dataframe, columns: list):
+    wiskers = {}
+    for column in columns:
+        interquartile_range = dataframe[column].describe()['75%'] - dataframe[column].describe()['25%']
+        upper_wisker = dataframe[column].describe()['75%'] + 1.5 * interquartile_range
+        lower_wisker = dataframe[column].describe()['25%'] - 1.5 * interquartile_range
+        wiskers[column] = [upper_wisker, lower_wisker]
+    return wiskers
+
+
+def set_extreme_threshold(wiskers: dict, threshold: dict):
+    for attribute in threshold:
+        if 'lower' in threshold[attribute]:
+            wiskers[attribute][1] = threshold[attribute]['lower']
+        elif 'upper' in threshold[attribute]:
+            wiskers[attribute][0] = threshold[attribute]['upper']
+    return wiskers
+
+
+def get_outlier_index(dataframe, columns: list, wiskers: dict, outlier_indexes=None, directions={}):
+    """If directions=None both values below lower wisker and above upper wisker will be considered outliers."""
+    if not outlier_indexes:
+        outlier_indexes = set()
+    for column in columns:
+        upper_wisker, lower_wisker = wiskers[column]
+        if column in directions and directions[column] == 'upper':
+            outlier_index = dataframe[dataframe[column] > upper_wisker].index
+        elif column in directions and directions[column] == 'lower':
+            outlier_index = dataframe[dataframe[column] < lower_wisker].index
+        elif column not in directions:
+            outlier_index = dataframe[(dataframe[column] < lower_wisker) | (dataframe[column] > upper_wisker)].index
+        outlier_indexes.update(outlier_index)
+    return outlier_indexes
